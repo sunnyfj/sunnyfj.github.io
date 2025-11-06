f5b8e8c3b6c41c40065d85e6078989350ca572d5
fff <sunnyfj@163.com>
2025-11-06 16:41:10 +0800

    feat(router): 添加 NProgress 进度条功能

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,8 @@
+import NProgress from 'nprogress'
 import { createRouter, createWebHistory } from 'vue-router'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({ showSpinner: false })
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +13,13 @@ const router = createRouter({
       component: () => import('@/views/index.vue'),
     },
   ],
+})
+
+router.beforeEach(() => {
+  NProgress.start()
+})
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
