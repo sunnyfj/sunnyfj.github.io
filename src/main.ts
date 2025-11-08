79d2ff227a3646685910f89afad56dc68afe1e00
fff <sunnyfj@163.com>
2025-11-08 16:39:45 +0800

    refactor(styles): 移除element.scss并调整样式导入结构

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,26 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createHead } from '@unhead/vue/client'
 
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 
 import router from './router'
+
+import 'dayjs/locale/zh-cn'
+
+import 'element-plus/dist/index.css'
 import '@/styles/reset.css'
 import 'virtual:uno.css'
-import '@/styles/element.scss'
 
-import '@/styles/index.scss'
+import '@/styles/index.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zhCn })
 app.use(createPinia())
 const head = createHead()
 app.use(head)
